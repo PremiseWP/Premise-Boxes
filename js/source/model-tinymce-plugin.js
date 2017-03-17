cdb14bd3df5eb538c1b6818f74822ef395caee14
Mario Vallejo <mario@vallgroup.com>
2017-03-17 18:40:57 -0500

    fixed issue: 'undefined' was added to the content of the box when editing a box that was previously empty (had no content).

--- js/source/model-tinymce-plugin.js
+++ js/source/model-tinymce-plugin.js
@@ -1,42 +1,42 @@
+/**
+ * Premise Boxes TinyMCE Class
+ *
+ * This handles all the functionality for our tinymce plugin and registers it's template
+ */
 (function($){
 	$(document).ready(function(){
 
 		var pboxesDialog   = $( '#pboxes_dialog' ),
-		pboxesDialogClose = pboxesDialog.find( '.pboxes-dialog-close' ),
-		pboxWrapper        = CodeMirror.fromTextArea( $('#pbox_wrapper')[0], {theme: 'monokai'} );
+		pboxesDialogClose  = pboxesDialog.find( '.pboxes-dialog-close' ),
+		pboxWrapper        = CodeMirror.fromTextArea( $('#pbox_wrapper')[0], {theme: 'monokai'} ),
+		shortcode_string   = 'pwp_boxes';
 
-		var media = wp.media, shortcode_string = 'pwp_boxes';
-		wp.mce = wp.mce || {};
+		wp.media = wp.media || {};
+		wp.mce   = wp.mce || {};
+
+		// our plugin
 		wp.mce.pwp_boxes = {
+
+			// holds data from our shortcode
 			shortcode_data: {},
-			template: media.template( 'editor-pwp-boxes' ),
+
+			// set our template
+			template: wp.media.template( 'editor-pwp-boxes' ),
+
 			getContent: function() {
 				var options = this.shortcode.attrs.named;
 				options.pbox_innercontent = this.shortcode.content;
 				return this.template(options);
 			},
-			View: { // before WP 4.2:
-				template: media.template( 'editor-pwp-boxes' ),
-				postID: $('#post_ID').val(),
-				initialize: function( options ) {
-					this.shortcode = options.shortcode;
-					wp.mce.pwp_boxes.shortcode_data = this.shortcode;
-				},
-				getHtml: function() {
-					var options = this.shortcode.attrs.named;
-					options.pbox_innercontent = this.shortcode.content;
-					return this.template(options);
-				}
-			},
+
 			edit: function( data ) {
 				var shortcode_data = wp.shortcode.next(shortcode_string, data);
 				var values = shortcode_data.shortcode.attrs.named;
-				values.pbox_innercontent = shortcode_data.shortcode.content;
+				values.pbox_innercontent = shortcode_data.shortcode.content || '';
 				wp.mce.pwp_boxes.popupwindow(tinyMCE.activeEditor, values);
 			},
 
 			// this is called from our tinymce plugin, also can call from our "edit" function above
-			// wp.mce.pwp_boxes.popupwindow(tinyMCE.activeEditor, "bird");
 			popupwindow: function( editor, values, onsubmit_callback ) {
 				editor = editor || wpActiveEditor;
 				values = values || {}; // console.log( values );
@@ -211,6 +211,7 @@
 			}
 		}; // pwp_boxes
 
+		// register the tinymce view template
 		wp.mce.views.register( shortcode_string, wp.mce.pwp_boxes );
 	});
 }(jQuery));
